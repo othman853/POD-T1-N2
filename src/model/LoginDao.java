55ac44c45ad332d58faa38f50ac8d09d911d501a
othman853 <anuar.yasser@gmail.com>
2015-06-12 03:02:40 -0300

    Implementando Dao do login às ganhas, só falta testar

--- src/model/LoginDao.java
+++ src/model/LoginDao.java
@@ -1,40 +1,51 @@
 package model;
 
 import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.ArrayList;
-import java.util.List;
 
 public class LoginDao extends GenericDao implements Dao<Login>{
 	
-	private static final String SAVE = "INSERT INTO login () VALUES ()";
-	private static final String DELETE = "DELETE FROM login WHERE";
-	private static final String FIND = "SELECT  FROM login WHERE";
-	private static final String FIND_ALL = "SELECT FROM login ";
+	private static final String SAVE = "INSERT INTO usuarios (cd_usuario, senha) VALUES (?, ?)";
+	private static final String DELETE = "DELETE FROM usuarios WHERE cd_usuario = ? and senha = ?";
+	private static final String FIND = "SELECT * FROM usuarios WHERE cd_usuario = ? and senha = ?";
+	private static final String FIND_ALL = "SELECT FROM usuarios ";
 
 	@Override
 	public void save(Login obj) throws Exception {
-		executeSQL(SAVE, parameters);		
+		executeSQL(SAVE, obj.getUsuario(), obj.getSenha());		
 	}
 
 	@Override
 	public void delete(Login obj) throws Exception {
-		executeSQL(DELETE, parameters);
+		executeSQL(DELETE, obj.getUsuario(), obj.getSenha());
 	}
 
 	@Override
-	public Login find(Login obj) throws Exception {
-		executeQuery(FIND, parameters);
-		return null;
+	public Login find(Login obj) throws Exception {		
+		return (Login) tratarResultSet(executeQuery(FIND, obj.getUsuario(), obj.getSenha())).get(0);
 	}
 
 	@Override
-	public ArrayList<?> findAll() throws Exception {
-		return tratarResultSet(executeQuery(FIND_ALL));
-		
+	public ArrayList<Login> findAll() throws Exception {
+		return tratarResultSet(executeQuery(FIND_ALL));		
 	}
 	
-	private ArrayList<?> tratarResultSet(ResultSet result){
+	private ArrayList<Login> tratarResultSet(ResultSet result){
 		
+		ArrayList<Login> logins = new ArrayList<>();
+		
+		try {
+			while(result.next()){
+				Login login = new Login(result.getString("cd_usuario"), result.getString("senha"));
+				logins.add(login);
+			}
+		} catch (SQLException e) {
+			System.out.println("[SQL ERROR]" + e.getMessage());
+			e.printStackTrace();
+		}
+		
+		return logins;	
 	}
 
 }
