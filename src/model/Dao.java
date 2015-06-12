999756fca0a7f553d3fad2a77f47d013402b8ae3
othman853 <anuar.yasser@gmail.com>
2015-06-12 03:01:12 -0300

    Corrigindo tretas do bam bam

--- src/model/Dao.java
+++ src/model/Dao.java
@@ -6,6 +6,6 @@ public interface Dao<T> {
 	public void save(T obj) throws Exception;
 	public void delete(T obj) throws Exception;
 	public T find(T obj) throws Exception;
-	public ArrayList<?> findAll() throws Exception;
+	public ArrayList<T> findAll() throws Exception;
 
 }
